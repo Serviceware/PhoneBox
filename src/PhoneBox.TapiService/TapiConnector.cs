1ffcbeee66097df68f3923d63cb5bd3a0fa44eef
Luben <ben_hauman@yahoo.com>
2022-08-12 00:04:09 +0200

    TAPI3Lib+

--- src/PhoneBox.TapiService/TapiConnector.cs
+++ src/PhoneBox.TapiService/TapiConnector.cs
@@ -8,17 +8,26 @@ namespace PhoneBox.TapiService
 {
     public sealed class TapiConnector : IHostedService, ITelephonyConnector
     {
+        private TAPI3Lib.TAPI? _tapiClient;
         public TapiConnector()
         {
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
+            _tapiClient = new TAPI3Lib.TAPIClass();
+            _tapiClient.Initialize();
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
+            var tmp = _tapiClient;
+            _tapiClient = null;
+            if (tmp != null)
+            {
+                tmp.Shutdown();
+            }
             return Task.CompletedTask;
         }
     }
