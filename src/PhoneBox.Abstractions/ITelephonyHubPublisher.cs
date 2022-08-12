6923afee37fa88be0b315154e90eb7cb89c13b27
Tommy <info@flexforge.me>
2022-08-12 10:00:32 +0200

    TapiConnector

--- src/PhoneBox.Abstractions/ITelephonyHubPublisher.cs
+++ src/PhoneBox.Abstractions/ITelephonyHubPublisher.cs
@@ -6,14 +6,4 @@ namespace PhoneBox.Abstractions
     {
         Task OnCall(CallSubscriber subscriber, string phoneNumber);
     }
-
-    public readonly struct CallSubscriber
-    {
-        public string PhoneNumber { get; }
-
-        public CallSubscriber(string phoneNumber)
-        {
-            this.PhoneNumber = phoneNumber;
-        }
-    }
 }
--- src/PhoneBox.TapiService/TapiConnector.cs
+++ src/PhoneBox.TapiService/TapiConnector.cs
@@ -1,57 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using PhoneBox.Abstractions;
+using TAPI3Lib;
 
 namespace PhoneBox.TapiService
 {
     public sealed class TapiConnector : IHostedService, ITelephonyConnector
     {
         private readonly ITelephonyHubPublisher _hubPublisher;
-        private TAPI3Lib.TAPI? _tapiClient;
+        private readonly IDictionary<CallSubscriber, Func<string, Task>> _registrations;
+        private TAPIClass? _tapiClient;
+        private CallNotification? _callNotification;
 
         public TapiConnector(ITelephonyHubPublisher hubPublisher)
         {
             this._hubPublisher = hubPublisher;
+            this._registrations = new Dictionary<CallSubscriber, Func<string, Task>>();
         }
 
         public void Register(CallSubscriber subscriber)
         {
-            Subscribe(phoneNumber => this._hubPublisher.OnCall(subscriber, phoneNumber));
+            this._registrations.Add(subscriber, (phoneNumber => this._hubPublisher.OnCall(subscriber, phoneNumber)));
         }
 
         Task IHostedService.StartAsync(CancellationToken cancellationToken)
         {
-            _tapiClient = new TAPI3Lib.TAPI();
+            _tapiClient = new TAPIClass();
             _tapiClient.Initialize();
+            _callNotification = new CallNotification(this._registrations);
+            this._tapiClient!.ITTAPIEventNotification_Event_Event += _callNotification.Event;
             return Task.CompletedTask;
         }
 
         Task IHostedService.StopAsync(CancellationToken cancellationToken)
         {
-            var tmp = _tapiClient;
-            _tapiClient = null;
-            if (tmp != null)
-            {
-                tmp.Shutdown();
-            }
+            this._tapiClient!.ITTAPIEventNotification_Event_Event -= _callNotification!.Event;
+            this._tapiClient!.Shutdown();
             return Task.CompletedTask;
         }
 
-        private void Subscribe(Func<string, Task> onCall)
+        private sealed class CallNotification : ITTAPIEventNotification
         {
-            // TODO: Subscribe TAPI
-        }
-    }
+            private readonly IDictionary<CallSubscriber, Func<string, Task>> _registrations;
 
-    internal readonly struct Caller
-    {
-        public string PhoneNumber { get; }
+            public CallNotification(IDictionary<CallSubscriber, Func<string, Task>> registrations)
+            {
+                this._registrations = registrations;
+            }
 
-        public Caller(string phoneNumber)
-        {
-            this.PhoneNumber = phoneNumber;
+            public async void Event(TAPI_EVENT TapiEvent, object pEvent)
+            {
+                if (TapiEvent != TAPI_EVENT.TE_CALLNOTIFICATION)
+                {
+                    return;
+                }
+
+                ITCallNotificationEvent notificationEvent = (ITCallNotificationEvent)pEvent;
+                CallSubscriber subscriber = new CallSubscriber(notificationEvent.Call.Address.AddressName);
+                if (this._registrations.TryGetValue(subscriber, out Func<string, Task> handler))
+                {
+                    string phoneNumber = CallInfoAsText(TapiEvent, notificationEvent.Call);
+                    await handler(phoneNumber)
+                }
+
+            }
+            private static string CallInfoAsText(TAPI_EVENT te, ITCallInfo callInfo, string txt = "")
+            {
+                int callid = callInfo.get_CallInfoLong(CALLINFO_LONG.CIL_CALLID);
+                string callerNumber = callInfo.get_CallInfoString(CALLINFO_STRING.CIS_CALLERIDNUMBER);
+                //string callerName = callInfo.get_CallInfoString(CALLINFO_STRING.CIS_CALLERIDNAME);
+
+                return te
+                    + " #" + callid
+                    + " S:" + callInfo.CallState
+                    + ", P:[" + callInfo.Privilege + "]"
+                    + ", Cu:[" + callerNumber + "]"
+                    //+ ", Ca:[" + callerName + "]"
+                    + " " + txt
+                    + ".";
+            }
         }
     }
 }
