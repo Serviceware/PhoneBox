aab5e4f44785af4a61b33462925e034eae688f07
Tommy <info@flexforge.me>
2022-08-30 08:32:20 +0200

    Make it work on macOS using VSCode

--- src/PhoneBox.Server/Program.cs
+++ src/PhoneBox.Server/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
